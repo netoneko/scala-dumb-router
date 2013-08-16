18960093dc26e7de63107d3eeba797b39ddc5f74
Kirill Maksimov <k.maksimov@applicaster.com>
2013-08-16 15:04:49 +0300

    Set status code for 404

--- src/main/scala/dumb/router/NotFoundHandler.scala
+++ src/main/scala/dumb/router/NotFoundHandler.scala
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse
 
 class NotFoundHandler extends Handler {
   override def handle(request: HttpServletRequest, response: HttpServletResponse) {
+    response.setStatus(404)
+
     val responseBody: NodeSeq = <html><body><h1>404</h1></body></html>
     response.getWriter.write(responseBody.toString)
   }
