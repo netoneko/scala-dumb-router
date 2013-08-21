0bc55e12369ee393e5656c8a8ad57b75177df7cf
Kirill Maksimov <k.maksimov@applicaster.com>
2013-08-22 01:23:30 +0300

    Handle immutable and mutable params separately

--- src/main/scala/dumb/router/impl/RealRequest.scala
+++ src/main/scala/dumb/router/impl/RealRequest.scala
@@ -2,14 +2,16 @@ package dumb.router.impl
 
 import javax.servlet.http.HttpServletRequest
 import dumb.router.Request
-import scala.collection.JavaConversions._
 
 case class RealRequest(req: HttpServletRequest) extends Request {
   val uri = req getRequestURI
 
-  val params = {
-    val hash = new java.util.HashMap[String, Array[String]]
-    hash.putAll(req.getParameterMap())
-    mapAsScalaMap(hash)
+  val params = collection.mutable.Map[String, Array[String]]()
+
+  val immutableParams = req.getParameterMap()
+
+  override def getParameterValues(key: String) = {
+    val values = immutableParams.get(key)
+    if (values == null) params(key) else values
   }
 }
